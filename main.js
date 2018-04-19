546750457038d84650edbcd334424a2b5239142d
mddesignstl <mddesignstl@gmail.com>
2018-04-19 17:43:32 -0500

    code corrections to getAuthCode()

--- main.js
+++ main.js
@@ -1,41 +1,35 @@
 
 var dataCenterAuthUrl = 'https://auth.bullhornstaffing.com/oauth/authorize'
 
-
-fucntion getAuthUrl(dataCenterAuthUrl,client_id,redirect,username,password){
-
-var redirect_uri='';
-
-if (redirect) {
-  redirect_uri = 'redirect_uri='+redirect;
+//Assemble the Auth url to be used in the Auth Call
+function getAuthUrl(dataCenterAuthUrl, client_id, redirect, username, password){
+  var redirect_uri='';
+  if (redirect) {
+    redirect_uri = 'redirect_uri='+redirect;
+  }
+  var authUrl=dataCenterAuthUrl +
+      '?action=Login&response_type=code&' +
+      'client_id='+client_id+'&' +
+      'username='+username+'&' +
+      'password='+password+'&' +
+      redirect_uri;
+  return authUrl;
 }
 
-var authUrl=dataCenterAuthUrl +
-    '?action=Login&response_type=code&' +
-    'client_id='+client_id+'&' +
-    'username='+username+'&' +
-    'password='+password+'&' +
-    redirect_uri
-
-return authUrl;
-}
-
+//Use the AuthUrl to retrive and return Auth Code
 function getAuthCode(authUrl){
   var code='';
   var xhr = new XMLHttpRequest();
-  xhr.read = function () {
+  xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         responseUrl = xhr.responseUrl;
-
-        var str     = responseUrl
-var matches = str.match(/code=([^&]*));
-var = (matches[1]);
-
+        var matches = responseUrl.match(/code=([^&]*));
+        code = matches[1];
       }
     };
   xhr.open("GET",authUrl, True);
   xhr.send();
 
-
+return code;
 
 };
